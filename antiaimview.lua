21d0a5de1328a458c24b813da039bca251ca4706
rexon <wespinninonriley@gmail.com>
2025-07-04 23:11:50 -0400

    Update antiaimview.lua

--- antiaimview.lua
+++ antiaimview.lua
@@ -1,56 +1,80 @@
-local China = setmetatable({}, {
-   __index = function(Company, Price)
-       return game:GetService(Price)
-   end})
-   
-local Players = China.Players
-local ReplicatedStorage = China.ReplicatedStorage
+local China =
+                setmetatable(
+                {},
+                {
+                    __index = function(Company, Price)
+                        return game:GetService(Price)
+                    end
+                }
+            )
 
---//Variables
-local LocalPlayer = Players.LocalPlayer
-local Mouse =  LocalPlayer:GetMouse()
+            local Players = China.Players
+            local ReplicatedStorage = China.ReplicatedStorage
 
-local MainEvent = ReplicatedStorage:FindFirstChild("MainEvent") or nil
-local Tool = nil
+            local LocalPlayer = Players.LocalPlayer
+            local Mouse = LocalPlayer:GetMouse()
 
-Bypass = function(Entity)
-   Entity.ChildAdded:Connect(function(Child)
-       if Child:IsA("Tool") then
-         Tool = Child.Activated:Connect(function()
-               if MainEvent then
-                  MainEvent:FireServer("UpdateMousePos", Mouse.Hit.Position)
-               end
-           end)
-       end
-   end)
-end
+            local MainEvent = ReplicatedStorage:FindFirstChild("MainEvent") or nil
+            local Tool = nil
 
+            Bypass = function(Entity)
+                Entity.ChildAdded:Connect(
+                    function(Child)
+                        if Child:IsA("Tool") then
+                            Tool =
+                                Child.Activated:Connect(
+                                function()
+                                    if MainEvent then
+                                        MainEvent:FireServer("UpdateMousePos", Mouse.Hit.Position)
+                                    end
+                                end
+                            )
+                        end
+                    end
+                )
+            end
 
+            local Alive = function(Player)
+                return Player and Player.Character and Player.Character:FindFirstChild("Humanoid") and
+                    Player.Character:FindFirstChild("Head") or
+                    false
+            end
 
-local Alive = function(Player)
-   return Player and Player.Character and Player.Character:FindFirstChild("Humanoid") and Player.Character:FindFirstChild("Head") or false
-end
+            LocalPlayer.CharacterAdded:Connect(
+                function(Character)
+                    Bypass(Character)
+                end
+            )
 
-LocalPlayer.CharacterAdded:Connect(function(Character)
-   Bypass(Character)
-end)
+            if isEnabled then
+                if Alive(LocalPlayer) then
+                    LocalPlayer.Character.Humanoid:UnequipTools()
+                    Bypass(LocalPlayer.Character)
+                end
+            end
 
-if Alive(LocalPlayer) then
-   LocalPlayer.Character.Humanoid:UnequipTools()
-   Bypass(LocalPlayer.Character)
-end
+            local Hook
+            Hook =
+                hookmetamethod(
+                game,
+                "__namecall",
+                function(self, ...)
+                    local Args = {...}
+                    local Method = getnamecallmethod()
 
-local Hook
-Hook = hookmetamethod(game, "__namecall", function(self, ...)
-   local Args = {...}
-   local Method = getnamecallmethod()
+                    if
+                        not checkcaller() and Method == "FireServer" and self.Name == "MainEvent" and
+                            Args[1] == "UpdateMousePos"
+                     then
+                        if isEnabled then
+                            Args[2] = "Anti Aim Viewer" and Mouse.Hit.Position
+                        end
+                        return self.FireServer(self, unpack(Args))
+                    end
 
-   if not checkcaller() and Method == "FireServer" and self.Name == "MainEvent" and Args[1] == "UpdateMousePos" then
-
-      Args[2] = "unitedchina made this" and Mouse.Hit.Position
-       
-       return self.FireServer(self, unpack(Args))
-   end
-   
-   return Hook(self, ...)
-end)
+                    return Hook(self, ...)
+                end
+            )
+        end
+    }
+)
